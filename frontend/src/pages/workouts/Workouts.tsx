98320593bcf97fd342ef28d2ef8cc4d16e3220e4
thomasdroege <ThomasDroege@gmx.de>
2022-11-14 19:05:01 +0100

    introduced typescript, fixed workout list

--- frontend/src/pages/workouts/Workouts.tsx
+++ frontend/src/pages/workouts/Workouts.tsx
@@ -4,38 +4,41 @@ import { DataGrid } from '@mui/x-data-grid';
 import {DeleteOutline} from '@material-ui/icons'
 import {userRows} from "../../dummyData"
 import {Link} from "react-router-dom"
+import axios, {AxiosResponse} from 'axios';
+import { couldStartTrivia } from "typescript";
 
 export default function Workouts() {
   const [isLoaded,setIsLoaded] = useState(false);
-  const [id, setId]=useState('');
-  const [timeStarted, setTimeStarted]=useState('');
-  const [workoutName, setWorkoutName]=useState('');
+  const [workouts, setWorkouts] = useState<Workouts[]>([]);
+  
 
+interface Workouts{
+  id: string;
+  timeStarted: string;
+  workoutName: string;
+  date: string;
+}
 
-  const [workouts, setWorkouts] = useState([]);
-  const workout={id, timeStarted, workoutName}
 
 useEffect(() => {
-  const axios = require('axios').default;
-  axios
-  .get('http://localhost:8080/workout/getAll')
-  .then((response) => {
-    setIsLoaded(true);
+  axios.get<Workouts[]>('http://localhost:8080/workout/getAll')
+  .then(response => {
     console.log(response.data);
-    
-    response.data.forEach(oneWorkout => {
-       workout.id = oneWorkout.id
-       workout.timeStarted = oneWorkout.timeStarted
-       workout.workoutName = oneWorkout.id
-    });
-    setWorkouts(response.data);
+    setIsLoaded(true);
+    response.data.forEach(test => {
+      test.date = test.timeStarted.substring(0,10);
+      test.timeStarted = test.timeStarted.substring(11,16);
+      test.workoutName = "Chest/Triceps";
+    })
+    setWorkouts(response.data); 
   });
 }, [])
 
 const columns = [
   {field: "id", headerName: "ID", width: 90},
+  {field: "workoutName", headerName: "WorkoutName", width: 200},
   {field: "timeStarted", headerName: "TimeStarted", width: 200},
-  {field: "workoutName", headerName: "WorkoutName", width: 200}
+  {field: "date", headerName: "Date", width: 200}
 ]
    
 return (
